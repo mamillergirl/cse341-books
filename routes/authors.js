9a34d04c8ff17d6ad6fb4cef90f70a99123c7001
mamillergirl <90581581+mamillergirl@users.noreply.github.com>
2023-06-10 15:44:03 -0700

    more comments for render

--- routes/authors.js
+++ routes/authors.js
@@ -2,12 +2,23 @@ const express = require("express");
 const authorsController = require("../controllers/authors");
 const validation = require("../middleware/validation");
 const router = express.Router();
-const auth = require("../middleware/auth")
+//const auth = require("../middleware/auth")
 
-router.get("/", auth.ensureAuth, authorsController.getAll);
-router.get("/:id", auth.ensureAuth, authorsController.getOne);
-router.post("/", auth.ensureAuth, validation.createAuthor, authorsController.createOne);
-router.put("/:id", auth.ensureAuth,  validation.createAuthor, authorsController.updateOne);
-router.delete("/:id", auth.ensureAuth, authorsController.deleteOne);
+router.get("/", 
+//auth.ensureAuth, 
+authorsController.getAll);
+router.get("/:id", 
+//auth.ensureAuth,
+ authorsController.getOne);
+router.post("/",
+ //auth.ensureAuth, 
+ validation.createAuthor, 
+authorsController.createOne);
+router.put("/:id",
+ //auth.ensureAuth,  
+validation.createAuthor, authorsController.updateOne);
+router.delete("/:id",
+ //auth.ensureAuth, 
+authorsController.deleteOne);
 
 module.exports = router;
--- routes/books.js
+++ routes/books.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const validation = require("../middleware/validation");
 const booksController = require("../controllers/books");
-const auth = require("../middleware/auth")
+//const auth = require("../middleware/auth")
 const router = express.Router();
 
-router.get("/", auth.ensureAuth, booksController.getAll);
-router.get("/:id", auth.ensureAuth,  booksController.getOne);
-router.post("/", auth.ensureAuth, validation.createBook, booksController.createOne);
-router.put("/:id", auth.ensureAuth, validation.createBook, booksController.updateOne);
-router.delete("/:id", auth.ensureAuth, booksController.deleteOne);
+router.get("/",
+ //auth.ensureAuth, 
+ booksController.getAll);
+router.get("/:id", 
+//auth.ensureAuth, 
+ booksController.getOne);
+router.post("/", 
+//auth.ensureAuth, 
+validation.createBook, booksController.createOne);
+router.put("/:id", 
+//auth.ensureAuth, 
+validation.createBook, booksController.updateOne);
+router.delete("/:id",
+//auth.ensureAuth,
+ booksController.deleteOne);
 
 module.exports = router;
