b5cdce67fd30d67608870f932e7ff24b8a4e4507
mamillergirl <90581581+mamillergirl@users.noreply.github.com>
2023-06-10 15:41:25 -0700

    comment for render

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,19 @@
 const express = require("express");
-const auth = require("../middleware/auth")
+//const auth = require("../middleware/auth")
 const router = express.Router();
 
 
 
 router.use('/', require('./swagger'))
-router.use("/books", auth.ensureAuth, require("./books"));
-router.use("/authors", auth.ensureAuth, require("./authors"));
-router.use("/auth",  require("./auth"));
-router.use("/profile", auth.ensureAuth, require("./profile"));
+router.use("/books",
+ //auth.ensureAuth, 
+ require("./books"));
+router.use("/authors", 
+//auth.ensureAuth, 
+require("./authors"));
+router.use("/auth",  
+require("./auth"));
+router.use("/profile", 
+//auth.ensureAuth,
+ require("./profile"));
 module.exports = router;
