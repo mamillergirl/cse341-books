552e232773ee820eaada126b1eec57a39ec8d1a4
mamillergirl <90581581+mamillergirl@users.noreply.github.com>
2023-12-30 18:42:21 -0700

    update callback

--- routes/auth.js
+++ routes/auth.js
@@ -13,7 +13,7 @@ passport.use(
       // options for google strategy
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "/auth/callback"
+      callbackURL: "https://cse341-books.onrender.com/auth/callback"
   }, (accessToken, refreshToken, profile, done) => {
       
       //console.log('passport callback function fired:');
