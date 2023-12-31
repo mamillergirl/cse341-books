f1916b1560e8eaa1848c2887fd5347223eb48d66
mamillergirl <90581581+mamillergirl@users.noreply.github.com>
2023-12-30 18:05:13 -0700

    update links

--- routes/profile.js
+++ routes/profile.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get("/", auth.ensureAuth, 
 //profileController.getProfile
-(req, res) => {res.send(`<h2>Welcome ${req.user.displayName}</h2><ul><li><a href="http://localhost:8080/api-docs">View Documentation</a></li><li><a href="http://localhost:8080/authors">View Authors</a></li><li><a href="http://localhost:8080/books">View Books</a></li></ul><button><a href="http://localhost:8080/auth/logout">Log Out</a></button>`);}
+(req, res) => {res.send(`<h2>Welcome ${req.user.displayName}</h2><ul><li><a href="/api-docs">View Documentation</a></li><li><a href="/authors">View Authors</a></li><li><a href="/books">View Books</a></li></ul><button><a href="/auth/logout">Log Out</a></button>`);}
 )
     
 
